e0e3969efa2099d0a6c81186296d2b91acf90da0
xuanry <xuanry@uw.edu>
2020-01-28 15:46:18 -0800

    Update chapter 10 exercise 4

--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -1,39 +1,43 @@
 # Exercise 4: external data sets: Gates Foundation Educational Grants
 
-# Use the `read.csv()` functoin to read the data from the `data/gates_money.csv`
+# Use the `read.csv()` function to read the data from the `data/gates_money.csv`
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- grants[, "organization"]
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization) # TRUE
 
 ## Now you can ask some interesting questions about the dataset
-
+colnames(grants)
 # What was the mean grant value?
-
+mean_grant <- mean(grants[, "total_amount"])
 
 # What was the dollar amount of the largest grant?
-
+largest_grant <- max(grants[, "total_amount"])
 
 # What was the dollar amount of the smallest grant?
-
+smallest_grant <- min(grants[, "total_amount"])
 
 # Which organization received the largest grant?
+largest_grant_org <- grants[grants$total_amount == max(grants$total_amount), "organization"]
 
-
-# Which organization received the smallest grant?
-
+# Which organization received the smallest grant, and how much was that?
+smallest_grant_org <- grants[grants$total_amount == smallest_grant, c("organization", "total_amount")]
 
 # How many grants were awarded in 2010?
+nrow(grants[grants$start_year == "2010", ])
+
+# How much grant were awarded in 2010?
+sum(grants[grants$start_year == "2010", "total_amount"])
 
