ae6c41b45920d1542e003e2910b4c70635f840e1
xuanry <xuanry@uw.edu>
2020-02-01 00:41:57 -0800

    complete chapter 11 exercise 4

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -3,6 +3,11 @@
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
 
+# install.packages("dplyr")
+library(dplyr)
+# install.packages("nycflights13")
+library(nycflights13)
+
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
@@ -10,33 +15,49 @@
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
 
+# ??flights
+View(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, delayed_in_air = arr_delay - dep_delay)
+View(flights)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- arrange(flights, -delayed_in_air)
+View(flights)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+flights <-  flights %>% 
+  mutate(delayed_in_air = arr_delay - dep_delay) %>% 
+  arrange(-delayed_in_air)
+View(flights)
 
 # Make a histogram of the amount of time gained using the `hist()` function
 
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+summarize(flights, ave = mean(delayed_in_air, na.rm = TRUE))
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
 
+# to_sea <- filter(flights, dest == "SEA")
+# to_sea <- select(to_sea, origin, dest, delayed_in_air)
+# View(to_sea)
+
+to_sea <- flights %>% 
+  filter(dest == "SEA") %>% 
+  summarize(avg_delay = mean(delayed_in_air, na.rm = TRUE)) %>% 
+  pull(avg_delay)
+View(to_sea)
 
 # On average, did flights to SeaTac gain or loose time?
-
+summarize(to_sea, avg_delay = mean(delayed_in_air, na.rm = TRUE))
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
